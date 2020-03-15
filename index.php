1bac0a0967b45f96597ec6c1d767bc0188aa71ce
justin <shieh_justin@yahoo.com>
2020-03-15 15:30:15 -0700

    php view

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-
-<html lang="en">
-
-<head>
-</head>
-<body>
-    <header>
-        <h1>   
-            <?php echo "hello world" ?> 
-        </h1>
-    </header>
-
-</body>
+<?php
 
 
-</html>
+$greeting = "test";
+
+require 'index.view.php';
---
+++ index.view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+</head>
+<body>
+    <header>
+        <h1>   
+            <?php 
+                echo "$greeting"; 
+            ?> 
+        </h1>
+    </header>
+
+</body>
+
+
+</html>
