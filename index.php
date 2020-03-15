f56d71f59013170b99325e4f92d0e83297a85d3c
justin <shieh_justin@yahoo.com>
2020-03-15 15:19:57 -0700

    php html

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+</head>
+<body>
+    <header>
+        <h1>   
+            <?php echo "hello world" ?> 
+        </h1>
+    </header>
+
+</body>
+
+
+</html>
