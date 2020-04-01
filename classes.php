0e2fca21839187d6252d0ed924241f9f77b28de3
justin <shieh_justin@yahoo.com>
2020-04-01 14:12:40 -0700

    laravel setup

--- classes.php
+++ classes.php
@@ -3,4 +3,16 @@
 class Comment {
     public $title;
     public $description;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function edit($comment) {
+        //
+    }
 }
+
+
+$test = new Comment('name');
